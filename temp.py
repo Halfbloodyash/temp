874167c09f19819f5062e417edc9872c7b1daf59
Halfbloodyash <91445689+Halfbloodyash@users.noreply.github.com>
2023-04-20 00:00:11 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,15 +7,13 @@ st.title('FMMS')
 st.header('Temp Data')
 df = pd.read_csv('temp.csv')
 chart = st.line_chart(df, x='Date/Time', y='Temperature')
-# while True:
-#     value = requests.get('https://api.init.st/data/v1/events/latest?accessKey=ist_6jxPkB-hjd9Iv2qkz5hzoxjhwwD9tUf-&bucketKey=piot_temp_stream031815', timeout=2)
-#     value = value.json()['temperature (C)']['value']
+while True:
+ value = requests.get('https://api.init.st/data/v1/events/latest?accessKey=ist_6jxPkB-hjd9Iv2qkz5hzoxjhwwD9tUf-&bucketKey=piot_temp_stream031815', timeout=2)
+ value = value.json()['temperature (C)']['value']
 
-#     dic = {'Date/Time': time.asctime(time.localtime())[4:19], 'Temperature': value}
-#     df = df.append(dic, ignore_index=True)
-#     df.to_csv('temp.csv',index=False)
-#     chart.empty()
-#     chart = st.line_chart(df, x='Date/Time', y='Temperature')
-
-    
-#     time.sleep(2)
+ dic = {'Date/Time': time.asctime(time.localtime())[4:19], 'Temperature': value}
+ df = df.append(dic, ignore_index=True)
+ df.to_csv('temp.csv',index=False)
+ chart.empty()
+ chart = st.line_chart(df, x='Date/Time', y='Temperature')
+ time.sleep(2)
