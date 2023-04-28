a61a62dd979f328b11a6924310d5ab899781fd78
Anish177 <30626499+Anish177@users.noreply.github.com>
2023-04-29 00:31:30 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -8,7 +8,7 @@ st.header('Temp Data')
 df = pd.read_csv('temp.csv')
 chart = st.line_chart(df, x='Date/Time', y='Temperature')
 while True:
- value = requests.get('https://api.init.st/data/v1/events/latest?accessKey=ist_6jxPkB-hjd9Iv2qkz5hzoxjhwwD9tUf-&bucketKey=piot_temp_stream031815', timeout=2)
+ value = requests.get('https://api.init.st/data/v1/events/latest?accessKey=ist_ZvMxedzZJYVKw5ZYEYzvPyZfuj03rmVi&bucketKey=Y8TK3CE9SPU5', timeout=2)
  value = value.json()['temperature (C)']['value']
 
  dic = {'Date/Time': time.asctime(time.localtime())[4:19], 'Temperature': value}
